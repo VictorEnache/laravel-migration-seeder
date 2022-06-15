df8b9b56b5922f595452cde9841c302c4918c6c5
VictorEnache <victor22enache@gmail.com>
2022-06-15 08:29:26 +0200

    fare il seeding

--- database/seeds/TripsTableSeeder.php
+++ database/seeds/TripsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Trip;
 use Illuminate\Database\Seeder;
 
 class TripsTableSeeder extends Seeder
@@ -11,6 +12,52 @@ class TripsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $trips = [
+            [
+                'destinazione' => 'Azzorre',
+                'prezzo' => 1000,
+                'giorni' => '7',
+                'data_partenza' => '12/06/2022',
+                'data_ritorno' => '19/06/2022',
+                'attivita' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis perferendis accusantium reiciendis earum? Culpa consectetur asperiores quas nihil, 
+                qui sed error unde libero ab omnis neque ipsa hic nesciunt!,',
+                'image' => 'https://picsum.photos/200/300'
+            ],
+
+            [
+                'destinazione' => 'MOldavia',
+                'prezzo' => 600,
+                'giorni' => '7',
+                'data_partenza' => '02/04/2022',
+                'data_ritorno' => '09/04/2022',
+                'attivita' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis perferendis accusantium reiciendis earum? Culpa consectetur asperiores quas nihil, 
+                qui sed error unde libero ab omnis neque ipsa hic nesciunt!,',
+                'image' => 'https://picsum.photos/200/300'
+            ],
+
+            [
+                'destinazione' => 'Malta',
+                'prezzo' => 2000,
+                'giorni' => '7',
+                'data_partenza' => '20/09/2022',
+                'data_ritorno' => '27/09/2022',
+                'attivita' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis perferendis accusantium reiciendis earum? Culpa consectetur asperiores quas nihil, 
+                qui sed error unde libero ab omnis neque ipsa hic nesciunt!,',
+                'image' => 'https://picsum.photos/200/300'
+            ]
+
+        ];
+
+        foreach($trips as $trip){
+            $new_trip = new Trip();
+            $new_trip->destinazione = $trip['destinazione'];
+            $new_trip->prezzo = $trip['prezzo'];
+            $new_trip->giorni = $trip['giorni'];
+            $new_trip->data_partenza = $trip['data_partenza'];
+            $new_trip->data_ritorno = $trip['data_ritorno'];
+            $new_trip->attivita = $trip['attivita'];
+            $new_trip->image = $trip['image'];
+            $new_trip->save();
+        }
     }
 }
