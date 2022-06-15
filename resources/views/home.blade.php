b19d180445b8f556c9377fe0f84e01954094d005
VictorEnache <victor22enache@gmail.com>
2022-06-15 07:13:50 +0200

    initial commit, struttura base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <p>qui va il contenuto della pagina</p>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <ul style="display:flex; list-style:none; justify-content:center; column-gap:1rem;">
+        <li>Home</li>
+        <li>Contacts</li>
+        <li>About</li>
+    </ul>
+</header>
